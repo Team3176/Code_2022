cee146ad8398f29353e584c0e9bdda356a301942
char@prometheus <char3176@gmail.com>
2022-02-03 22:24:56 -0500

    Converted frc.robot namespaces to team3176.robot namespaces.

---
+++ src/main/java/team3176/robot/subsystems/controller/Controller.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package team3176.robot.subsystems.controller;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import team3176.robot.constants.ControllerConstants;
+import team3176.robot.util.XboxAxisAsButton;
+import team3176.robot.util.XboxLoneButton;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Axis;
+import edu.wpi.first.wpilibj.XboxController.Button;
+
+public class Controller {
+
+  public Controller() {}
+
+}
---
+++ src/main/java/team3176/robot/subsystems/indexer/Indexer.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package team3176.robot.subsystems.indexer;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team3176.robot.constants.IndexerConstants;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Indexer extends SubsystemBase
+{
+  private static Indexer m_indexer = new Indexer();
+  private CANSparkMax indexerMotor1;
+  private SparkMaxPIDController pidController1;
+  private RelativeEncoder encoder1;
+  private RelativeEncoder encoder2;
+
+  public Indexer()
+  {
+    indexerMotor1 = new CANSparkMax(IndexerConstants.INDEXER_NEO1_CAN_ID, MotorType.kBrushless);
+    pidController1 = indexerMotor1.getPIDController();
+    encoder2 = indexerMotor1.getEncoder();
+    
+    indexerMotor1.setClosedLoopRampRate(IndexerConstants.kRampRate);
+  }
+
+  public void motor1Position(double position)
+  {
+    pidController1.setReference(position, ControlType.kPosition);
+  }
+
+  public void  motorStop() {
+    indexerMotor1.set(0.0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  public static Indexer getInstance() {
+    return m_indexer;
+  }
+
+}
