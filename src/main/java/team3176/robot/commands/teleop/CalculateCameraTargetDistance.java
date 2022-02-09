9d73c2cd409d3088da40218ba40422af05bb159a
SmartNerdGuy <smartnerdguy13@gmail.com>
2022-02-08 20:52:52 -0500

    Added vision code for testing purposes.

--- src/main/java/team3176/robot/commands/teleop/CalculateCameraTargetDistance.java
+++ src/main/java/team3176/robot/commands/teleop/CalculateCameraTargetDistance.java
@@ -6,21 +6,55 @@ import team3176.robot.subsystems.vision.Vision;
 
 public class CalculateCameraTargetDistance extends CommandBase {
 
-    private final Vision mSubsystem;    
+    private final Vision mSubsystem;
+    private double[] distances;
+    private double averageDistance;
 
     public CalculateCameraTargetDistance(Vision subsystem){
         mSubsystem = subsystem;
+        distances = new double[10];
+        averageDistance = 0;
         addRequirements(subsystem);
     }
 
     @Override
     public void execute(){
         double result = mSubsystem.calculateDeltaX();
-        SmartDashboard.putNumber("Camera Distance", result);
+        System.out.println("I RAN!!!");
+        addDistance(result);
+    }
+
+    private void addDistance(double newDistance){
+        System.out.println("I RAN TOO!!!");
+        for(int i = 0; i < 10; i++){
+            if(distances[i] == 0){
+                distances[i] = newDistance;
+            }
+        }
+    }
+
+    private boolean checkIfFinished(){
+        System.out.println("I RAN THREE!!!");
+        double sum = 0;
+        for(double currValue : distances){
+            if(currValue == 0){
+                System.out.println("I CAME BACK FALSE!!!");
+                return false;
+            }
+            sum += currValue;
+        }
+        averageDistance = sum / 10;
+        System.out.println("I CAME BACK TRUE!!!");
+        return true;
     }
 
     @Override
     public boolean isFinished(){
+        if(checkIfFinished()){
+            System.out.println("I FINISHED!!!");
+            SmartDashboard.putNumber("Average Distance", averageDistance);
+            return true;
+        }
         return false;
     }
 }
