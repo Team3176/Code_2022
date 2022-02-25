0b26b4c9f2a3bff3ef0489894fc8c38829ad7115
SwitchingPersona <emeraldrailway56@gmail.com>
2022-02-24 19:50:43 -0500

    Added Command group for two balls

--- src/main/java/team3176/robot/commands/common/ShootTwoBall.java
+++ src/main/java/team3176/robot/commands/common/ShootTwoBall.java
@@ -9,18 +9,16 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ShootOneBall extends SequentialCommandGroup {
-  /** Creates a new Shoot. */
-  public ShootOneBall() {
+public class ShootTwoBall extends SequentialCommandGroup {
+  /** Creates a new ShootTwoBall. */
+  public ShootTwoBall() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new IndexerPositionChange(010),
+      new IndexerPositionChange(110),
       // Angles
-      new IndexerPositionChange(001),
+      new IndexerPositionChange(011)
       // Start Feeder and Flywheels
-      new IndexerPositionChange(000)
-      // Stop Stuff
     );
   }
 }
