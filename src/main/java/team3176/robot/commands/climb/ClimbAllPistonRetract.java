5a1f9f796b8403a64c3d4b655bf6cd65f7cc9ec1
char@odin <char3176@gmail.com>
2022-02-27 16:16:43 -0500

    Converted climb package calls to ...commands.climb.<classname>

--- src/main/java/team3176/robot/commands/climb/ClimbAllPistonRetract.java
+++ src/main/java/team3176/robot/commands/climb/ClimbAllPistonRetract.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package team3176.robot.commands.Climb;
+package team3176.robot.commands.climb;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import team3176.robot.constants.MasterConstants;
