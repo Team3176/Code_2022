2bddecf6334896ecc401d607848b27ce6402d35f
JaredTheBro <jaredab370@gmail.com>
2022-02-28 19:51:44 -0500

    Whoops I modified the merge branch. Mostly just naming stuff, though.

--- src/main/java/team3176/robot/subsystems/Feeder.java
+++ src/main/java/team3176/robot/subsystems/Feeder.java
@@ -6,7 +6,7 @@ package team3176.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team3176.robot.constants.TransferConstants;
-import team3176.robot.subsystems.FeederIO.TransferIOInputs;
+import team3176.robot.subsystems.FeederIO.FeederIOInputs;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.littletonrobotics.junction.Logger; 
@@ -19,39 +19,39 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Feeder extends SubsystemBase
 {
-  private CANSparkMax transferMotor1;
-  private SparkMaxPIDController pidController1;
-  private RelativeEncoder encoder1;
+  private CANSparkMax feederMotor;
+  private SparkMaxPIDController pidController;
+  private RelativeEncoder encoder;
 
   private final FeederIO io;
-  private final TransferIOInputs inputs = new TransferIOInputs();
+  private final FeederIOInputs inputs = new FeederIOInputs();
   private static Feeder instance;
 
   public Feeder(FeederIO io)
   {
     this.io = io;
 
-    transferMotor1 = new CANSparkMax(TransferConstants.TRANSFER_NEO_CAN_ID, MotorType.kBrushless);
-    pidController1 = transferMotor1.getPIDController();
-    encoder1 = transferMotor1.getEncoder();
+    feederMotor = new CANSparkMax(TransferConstants.TRANSFER_NEO_CAN_ID, MotorType.kBrushless);
+    pidController = feederMotor.getPIDController();
+    encoder = feederMotor.getEncoder();
     
-    transferMotor1.setClosedLoopRampRate(TransferConstants.kRampRate);
+    feederMotor.setClosedLoopRampRate(TransferConstants.kRampRate);
   }
 
   public void percentOutput() 
   {
     double output = SmartDashboard.getNumber(TransferConstants.kShuffleboardPercentName, 0.0);
-    if (output >= -1 && output <= 1) { transferMotor1.set(output); }
-    SmartDashboard.putNumber("TransferRPMOut", encoder1.getVelocity());
+    if (output >= -1 && output <= 1) { feederMotor.set(output); }
+    SmartDashboard.putNumber("TransferRPMOut", encoder.getVelocity());
   }
 
-  public void motor2Velocity(double velocity)
+  public void setVelocityPID(double RPM)
   {
-    pidController1.setReference(velocity, ControlType.kVelocity);
+    pidController.setReference(RPM, ControlType.kVelocity);
   }
 
   public void stopMotor() {
-    transferMotor1.set(0.0);
+    feederMotor.set(0.0);
   }
 
   
