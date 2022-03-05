f3092cb8a81a0b7407383c0f302f5dbb393c1de4
Kyle <45471789+kphillips2003@users.noreply.github.com>
2022-03-04 20:24:58 -0500

    Position Based Shooting Values

---
+++ src/main/java/team3176/robot/commands/Shooter/FlywheelBackSpinSet.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package team3176.robot.commands.Shooter;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import team3176.robot.subsystems.Flywheel;
+import team3176.robot.subsystems.Vision;
+
+public class FlywheelBackSpinSet extends InstantCommand {
+  private Flywheel m_Flywheel = Flywheel.getInstance();
+  private Vision m_Vision = Vision.getInstance();
+  private double front;
+  private double back;
+
+  public FlywheelBackSpinSet(double front, double back) {
+    this.front = front;
+    this.back = back;
+    addRequirements(m_Flywheel);
+  }
+
+  @Override
+  public void initialize() {
+    m_Flywheel.spinMotors(this.front, this.back);
+  }
+}
