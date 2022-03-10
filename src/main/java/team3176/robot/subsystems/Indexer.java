1d2246f7f2949b854d21bc87b36ec661be9fc57e
char@odin <char3176@gmail.com>
2022-03-10 14:06:10 -0500

    Looking at alternate way to run Indexer

--- src/main/java/team3176/robot/subsystems/Indexer.java
+++ src/main/java/team3176/robot/subsystems/Indexer.java
@@ -30,10 +30,12 @@ public class Indexer extends SubsystemBase {
   private byte[] sensorByteArray;
   private boolean[] sensorBoolArray = new boolean[IndexerConstants.NUM_OF_SENSORS];
   private boolean isSmartDashboardTestControlsShown;
-  public String mode = "";
   private boolean firstPos, secondPos, thirdPos;
   private int lastState = 222;
   private int currentState = 222;
+
+  public enum Mode{INTAKING, HOLDING, SPITTING, SHOOTING};
+  private Mode mode;
 
   private final IndexerIO io;
   private final IndexerIOInputs inputs = new IndexerIOInputs();
@@ -48,8 +50,8 @@ public class Indexer extends SubsystemBase {
     indexerMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0); 
     this.indexerMotor.configNominalOutputForward(0, IndexerConstants.kTIMEOUT_MS);
     this.indexerMotor.configNominalOutputReverse(0, IndexerConstants.kTIMEOUT_MS);
-    // this.indexerMotor.configPeakOutputForward(0.25, IndexerConstants.kTIMEOUT_MS);
-    // this.indexerMotor.configPeakOutputReverse(-0.25, IndexerConstants.kTIMEOUT_MS);
+    // this.indexerMotor.configPeakOutputForward(0.8, IndexerConstants.kTIMEOUT_MS);
+    // this.indexerMotor.configPeakOutputReverse(-0.8, IndexerConstants.kTIMEOUT_MS);
     this.indexerMotor.configAllowableClosedloopError(IndexerConstants.kPID_LOOP_IDX, IndexerConstants.ALLOWABLE_CLOSED_LOOP_ERROR, IndexerConstants.kTIMEOUT_MS);
     this.indexerMotor.config_kF(IndexerConstants.kPID_LOOP_IDX, IndexerConstants.PIDFConstants[3], IndexerConstants.kTIMEOUT_MS);
     this.indexerMotor.config_kP(IndexerConstants.kPID_LOOP_IDX, IndexerConstants.PIDFConstants[0], IndexerConstants.kTIMEOUT_MS);
@@ -57,13 +59,64 @@ public class Indexer extends SubsystemBase {
     this.indexerMotor.config_kD(IndexerConstants.kPID_LOOP_IDX, IndexerConstants.PIDFConstants[2], IndexerConstants.kTIMEOUT_MS);
     this.indexerMotor.config_IntegralZone(IndexerConstants.kPID_LOOP_IDX, IndexerConstants.PIDFConstants[4], IndexerConstants.kTIMEOUT_MS); 
 
-
+    this.mode = Mode.HOLDING;
 
     //indexerMotor.setClosedLoopRampRate(IndexerConstants.RAMP_RATE);
 
 
     m_I2C = new I2C(I2C.Port.kMXP, IndexerConstants.I2C_DEVICE_ADDRESS);
   }
+
+  public void index(){
+    switch (this.mode) {
+      case INTAKING:
+        if (reportState() == 000) {
+          //Run motor continuously Up
+        }
+        if (reportState() == 100) {
+          //PID Position motor up to State010
+        }
+        if (reportState() == 010 && indexerMotor.getMotorOutputPercent() > 0) {
+          //stopMotor
+        }
+        if (reportState() == 110 && indexerMotor.getMotorOutputPercent() == 0) {
+          //PID Position motor up to State011
+        }
+
+        break;
+      case HOLDING:
+        if (reportState() == 100) {
+          //Run motor up to state010
+        }
+        if (reportState() == 001) {
+          //Run motor back to state010
+        }
+        if (reportState() == 011) {
+          //Not sure what to do here
+        }
+        if (reportState() == 101) {
+          //This is the state we want to hold in for 2 balls
+        }
+        if (reportState() == 110) {
+          //Not sure what to do here
+        }
+        break;
+      case SPITTING:
+        // if (intake is extended) and (intake motor getMotorOutputPercent() < 0) {
+            // run motor down at -.80
+        //}
+        break;
+      case SHOOTING:
+        //run motor Up at 80%
+        break;
+      default:
+        break;
+    }
+
+
+  }
+
+
 
   public void setIndexerPosition(double position) {
     indexerMotor.set(ControlMode.Position, position);
@@ -79,11 +132,11 @@ public class Indexer extends SubsystemBase {
 
   public int reportState() {
     int state = 0;
-    if (firstPos)
+    if (!firstPos)
       state += 100;
-    if (secondPos)
+    if (!secondPos)
       state += 10;
-    if (thirdPos)
+    if (!thirdPos)
       state++;
     
     return state;
@@ -139,6 +192,19 @@ public class Indexer extends SubsystemBase {
 
   public void setValuesFromSmartDashboard() {
     indexerMotor.set(ControlMode.PercentOutput, (SmartDashboard.getNumber("Indexer PCT", 0)));
+  }
+
+  public void setModeIntaking(){
+    this.mode = Mode.INTAKING;
+  }
+  public void setModeHolding(){
+    this.mode = Mode.HOLDING;
+  }
+  public void setModeSpitting(){
+    this.mode = Mode.SPITTING;
+  }
+  public void setModeShooting(){
+    this.mode = Mode.SHOOTING;
   }
 
   @Override
