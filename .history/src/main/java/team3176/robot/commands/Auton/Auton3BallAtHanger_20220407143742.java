0c00cd9dcd47d67e81d8df2c111791851b90c8c6
char@odin <char3176@gmail.com>
2022-04-07 14:39:17 -0400

    converted Autons to use TrapezoidRotate instead of AutoRotate.  Needs testing & fine tuning of angles passed to TrapezoidRotate.

--- .history/src/main/java/team3176/robot/commands/Auton/Auton3BallAtHanger_20220407143742.java
+++ .history/src/main/java/team3176/robot/commands/Auton/Auton3BallAtHanger_20220407143742.java
@@ -8,26 +8,43 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import team3176.robot.commands.CMD_Groups.AutoBallOneFlywheelAngle;
 import team3176.robot.commands.CMD_Groups.AutoShoot50;
+import team3176.robot.commands.CMD_Groups.FlywheelAngleWall;
 import team3176.robot.commands.Drivetrain.imported.TrapezoidDrive;
-import team3176.robot.commands.Drivetrain.imported.TrapezoidRotate;
 import team3176.robot.commands.Intake.IntakeExtendSpin;
 import team3176.robot.commands.Intake.IntakeRetractStop;
 import team3176.robot.commands.Shooter.AnglerZeroAtMax;
 import team3176.robot.commands.Drivetrain.imported.AutonRotate;
 import team3176.robot.commands.Drivetrain.imported.SwerveResetGyro;
 
-public class Auto2BallsAtHanger extends SequentialCommandGroup {
-  public Auto2BallsAtHanger() {
+
+public class Auton3BallAtHanger extends SequentialCommandGroup {
+  public Auton3BallAtHanger() {
     addCommands(
       new SwerveResetGyro(),
       new AnglerZeroAtMax(),
       new AutoBallOneFlywheelAngle(),
       new WaitCommand(0.5),
       new IntakeExtendSpin(),
-      new TrapezoidDrive(6, 0),  
-      new TrapezoidRotate(-1, 5), 
+      new TrapezoidDrive(6, 0),
+      new TrapezoidRotate(-1,4),   
       //new AutonRotate(-.15, 5),
       new IntakeRetractStop(),
+      new AutoShoot50(),
+      new WaitCommand(2),
+      new AutonStopShootParallel(),
+
+      /**3 Ball */
+
+      new FlywheelAngleWall(),
+      new TrapezoidRotate(-1,20),
+      //new AutonRotate(-.15, 80),
+      new IntakeExtendSpin(),
+      new TrapezoidDrive(26, 0),
+      new IntakeRetractStop(),
+     
+      new TrapezoidRotate(1,5),
+      //new AutonRotate(.15, 10),
+      new WaitCommand(3),
       new AutoShoot50(),
       new WaitCommand(2),
       new AutonStopShootParallel()
