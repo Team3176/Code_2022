9fd2b9dc873f3a257117c650e0922533d3333744
char@odin <char3176@gmail.com>
2022-04-16 00:49:37 -0400

    Leave me alone and let me make it work.  WIP.

--- src/main/java/team3176/robot/commands/Drivetrain/imported/AutonRotatePidGyro.java
+++ src/main/java/team3176/robot/commands/Drivetrain/imported/AutonRotatePidGyro.java
@@ -18,70 +18,38 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 public class AutonRotatePidGyro extends CommandBase {
   /** Creates a new TrapezoidDrive. */
   private Drivetrain m_Drivetrain = Drivetrain.getInstance();
-  private Gyro3176 m_gyro = Gyro3176.getInstance();
+  private Gyro3176 m_Gyro = Gyro3176.getInstance();
   private CoordSys m_CoordSys = CoordSys.getInstance();
-  private Timer timer = new Timer();
   private TrapezoidProfile profile;
-  private double theta;
-  private double distanceX;
-  private double distanceY;
-  private double rotation_distance, direction;
   private double botCircumferance = 12.315;  //feet
-  private double botCircumferancePer1Degree;
+  private double initial_yaw;
+  private double yaw_diff;
+  private double yaw_TrueSetpoint;
 
   /**
    * 
-   * @param direction  +1 makes it turn right, -1 makes it turn left
-   * @param angle  scales as follows
-   *                5=~10 degrees
-   *                10 = 45
-   *                20 = 90
-   *                22.5 = 135
-   *                25 = 180
-   *                27.5 = 225
-   *                32.5 - 35 = 270
-   *                42.5 - 45 = 325
-   *                52.5 = 360
+   * @param direction  
+   * @param angle  
    */
-  public AutonRotatePidGyro(double direction, double angle) { //TODO: FIIIIIIIIXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxx
+  public AutonRotatePidGyro(double yaw_setpoint) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_Drivetrain);
-    //this.distanceX = distanceX;
-    //this.distanceY = distanceY;
-    this.botCircumferancePer1Degree = botCircumferance / 360;
+    addRequirements(m_Gyro);
     m_CoordSys.setCoordTypeToFieldCentric();
     m_CoordSys.setCoordTypeToRobotCentric();
-    //double hyp = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-    //this.theta = Math.atan(distanceY / distanceX);
-    this.direction = -direction;
-    this.rotation_distance = angle * botCircumferancePer1Degree;
-    profile = new TrapezoidProfile
-    (new TrapezoidProfile.Constraints(14, 12), new TrapezoidProfile.State(rotation_distance, 0), new TrapezoidProfile.State(0, 0));
+    this.yaw_diff = yaw_setpoint;
+    this.yaw_TrueSetpoint = this.initial_yaw + yaw_setpoint;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_CoordSys.setCoordType(coordType.ROBOT_CENTRIC);
-    //m_gyro.setSpinLockAngle();
-    //m_gyro.setSpinLock(true);
-    timer.reset();
-    timer.start();
+    initial_yaw = m_Gyro.getYaw();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    TrapezoidProfile.State setPoint = profile.calculate(timer.get());
-    /// double velocityX = setPoint.velocity * Math.cos(theta);
-    //double velocityY = setPoint.velocity * Math.sin(theta);
-    double spinCommand = setPoint.velocity * rotation_distance;
-    if (this.direction > 0) {spinCommand *= -1;}
-    double smallnum = Math.pow(10,-10);
-    m_Drivetrain.drive(smallnum, smallnum, spinCommand);
-    System.out.println("auto trapeziod constants " + timer.get() + " : " + spinCommand);
-
-    // System.out.println("X: " + distanceX + ", Y: " + distanceY);
   }
 
   // Called once the command ends or is interrupted.
@@ -91,13 +59,13 @@ public class AutonRotatePidGyro extends CommandBase {
     //m_Drivetrain.drive(smallnum, smallnum, smallnum);
     //m_Drivetrain.drive(0,0,0);
     m_Drivetrain.stopMotors();
-    timer.stop();
     //System.out.println("######################################################################################################################     TrapRot.end()");
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return profile.isFinished(timer.get());
+    //return profile.isFinished(timer.get());
+    return false;
   }
 }
