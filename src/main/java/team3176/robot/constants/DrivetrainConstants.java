f58649d4cf133ccee5be6abcc45f82488132505e
char@odin <char3176@gmail.com>
2022-06-08 23:12:09 -0400

    Deleted 2021 constants from DrivetrainConstants.java & NavConstants.java

--- src/main/java/team3176/robot/constants/DrivetrainConstants.java
+++ src/main/java/team3176/robot/constants/DrivetrainConstants.java
@@ -33,13 +33,13 @@ public class DrivetrainConstants {
   
 
     // Drivetrain dimensions for kinematics and odometry
-    public static final double LENGTH_IN_METERS_2021 = 0.581; // measured in inches as 24.75
-    public static final double LENGTH_IN_INCHES_2021 = Units.metersToInches(LENGTH_IN_METERS_2021); //58.1 * (1.0/2.54); // 22.87 inches but measured in cm as 58.1cm
+    public static final double LENGTH_IN_METERS_2022 = 0.581; // measured in inches as 24.75
+    public static final double LENGTH_IN_INCHES_2022 = Units.metersToInches(LENGTH_IN_METERS_2022); //58.1 * (1.0/2.54); // 22.87 inches but measured in cm as 58.1cm
 
     public static final double WIDTH_IN_METERS_2022 = 0.61595; // measured in inches as 24.25in   24.75
     public static final double WIDTH_IN_INCHES_2022 = Units.metersToInches(WIDTH_IN_METERS_2022); //64.0 * (1.0/2.54); // 25.197 inches but measured in cm as 64.0cm
 
-    public static double LENGTH = LENGTH_IN_INCHES_2021;
+    public static double LENGTH = LENGTH_IN_INCHES_2022;
     public static double WIDTH = WIDTH_IN_INCHES_2022;
     public static double DRIVE_ENCODER_UNITS_PER_REVOLUTION;
 
@@ -100,8 +100,6 @@ public class DrivetrainConstants {
      */ 
 
      // Below line contains offset needed to rotate raw navx angle output such that +x=fore and +y=port
-    public static final double GYRO_ROTATIONAL_OFFSET_2019 = 0;/*Units.radiansToDegrees(Math.PI / 2); */  // Currently equivalent to +90 Degrees 
-    public static final double GYRO_ROTATIONAL_OFFSET_2021 =90; //-90;/*Units.radiansToDegrees(Math.PI / 2); */  // Currently equivalent to +90 Degrees 
     public static final double GYRO_ROTATIONAL_OFFSET_2022_practiceBot = 0; 
     public static final double GYRO_ROTATIONAL_OFFSET_2022_actualBot = 0; 
     public static final double GYRO_ROTATIONAL_OFFSET_FOR_RIO_MOUNTING = (MasterConstants.IS_PRACTICE_BOT) ? GYRO_ROTATIONAL_OFFSET_2022_practiceBot : GYRO_ROTATIONAL_OFFSET_2022_actualBot; 
