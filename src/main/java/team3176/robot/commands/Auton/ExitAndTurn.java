840d24a5af50e5af6c96e995b21a88306078672e
Jonathan Heidegger <jth3141@gmail.com>
2022-06-13 22:35:48 -0400

    basic swerve drive command created for auto

--- src/main/java/team3176/robot/commands/Auton/ExitAndTurn.java
+++ src/main/java/team3176/robot/commands/Auton/ExitAndTurn.java
@@ -1,6 +1,7 @@
 package team3176.robot.commands.Auton;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import team3176.robot.commands.Drivetrain.imported.SwerveTimedDrive;
 import team3176.robot.commands.Drivetrain.imported.TrapezoidDrive;
 import team3176.robot.commands.Drivetrain.imported.TrapezoidRotate;
 
@@ -8,7 +9,7 @@ public class ExitAndTurn extends SequentialCommandGroup {
   public ExitAndTurn() {
     addCommands(
       new TrapezoidDrive(10, 0),
-      new TrapezoidRotate(1, 70),
+      new SwerveTimedDrive(0.0, 0.0, .2).withTimeout(2),
       new TrapezoidDrive(10, 0));
   }
 }
