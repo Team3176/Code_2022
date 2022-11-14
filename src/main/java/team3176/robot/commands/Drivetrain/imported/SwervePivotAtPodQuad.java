69ff970aa5e556f2127f48cb6e8cf1611f58a63b
char@odin <char3176@gmail.com>
2022-11-13 23:28:17 -0500

    Post WPILib 2023 Import of Code_2022:SwervePodState

---
+++ src/main/java/team3176/robot/commands/Drivetrain/imported/SwervePivotAtPodQuad.java
@@ -0,0 +1,128 @@
+/*
+/*
+ 
+   ____  _            _           _     ____           _ 
+  |  _ \(_)_   _____ | |_    __ _| |_  |  _ \ ___   __| |
+  | |_) | \ \ / / _ \| __|  / _` | __| | |_) / _ \ / _` |
+  |  __/| |\ V / (_) | |_  | (_| | |_  |  __/ (_) | (_| |
+  |_|   |_| \_/ \___/ \__|  \__,_|\__| |_|   \___/ \__,_|
+                                                         
+ 
+*/
+package team3176.robot.commands.Drivetrain.imported;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import team3176.robot.constants.SwerveSubsystemConstants;
+import team3176.robot.subsystems.SwerveSubsystem.SwerveSubsystem;
+import team3176.robot.subsystems.SwerveSubsystem.CoordSys.coordType;
+import team3176.robot.subsystems.SwerveSubsystem.SwerveSubsystem.driveMode;
+import team3176.robot.subsystems.SwerveSubsystem.Gyro3176;
+import team3176.robot.subsystems.SwerveSubsystem.CoordSys;
+
+public class SwervePivotAtPodQuad extends CommandBase {
+  private SwerveSubsystem m_SwerveSubsystem = SwerveSubsystem.getInstance();
+  private Gyro3176 m_Gyro = Gyro3176.getInstance();
+  private CoordSys m_CoordSys = CoordSys.getInstance();
+  private DoubleSupplier forwardCommand, strafeCommand, spinCommand;
+  private Double hatPov, pov;
+
+  private double radianOffset;
+
+  public SwervePivotAtPodQuad(DoubleSupplier forwardCommand, DoubleSupplier strafeCommand, DoubleSupplier spinCommand, Double hatPov) {
+    this.forwardCommand = forwardCommand;
+    this.strafeCommand = strafeCommand;
+    this.spinCommand = spinCommand;
+    this.hatPov = hatPov;
+    this.pov = this.hatPov;
+    addRequirements(m_SwerveSubsystem, m_Gyro);
+  }
+
+  @Override
+  public void initialize() {
+    double mYaw = 0;
+    double yaw = m_Gyro.getYaw();
+
+    if (yaw < 0) { mYaw = yaw % -180.0; } else { mYaw = yaw % 180;}
+
+    if ((mYaw >= 45 && mYaw < 135)) {
+      switch (this.hatPov.intValue()) {
+        case 45:  this.pov = this.pov + 90.0;
+                  break;
+        case 135: this.pov = this.pov + 90.0;
+                  break;
+        case 225: this.pov = this.pov + 90.0;
+                  break;
+        case 315: this.pov = 45.0;
+                  break;
+      }
+    }
+    
+    if ((mYaw >= 135 && mYaw <= 180) || (mYaw < -135 && mYaw >= -180)) {
+      switch (this.hatPov.intValue()) {
+        case 45:  this.pov = this.pov + 180.0;
+                  break;
+        case 135: this.pov = this.pov + 180.0;
+                  break;
+        case 225: this.pov = 45.0;
+                  break;
+        case 315: this.pov = 135.0;
+                  break;
+      }
+    } 
+
+    if (mYaw >= -135 && mYaw < -45) {
+      switch (this.hatPov.intValue()) {
+        case 45:  this.pov = this.pov + 315.0;
+                  break;
+        case 135: this.pov = 45.0;
+                  break;
+        case 225: this.pov = 135.0;
+                  break;
+        case 315: this.pov = 225.0;
+                  break;
+      }
+    }
+
+
+
+    radianOffset = m_Gyro.getCurrentChassisYaw() - m_CoordSys.getFieldCentricOffset();
+
+  }
+
+  @Override
+  public void execute() {
+
+/*
+    if (pov == 45.0) {
+      m_SwerveSubsystem.setDriveMode(driveMode.PIVOTBL);
+    }
+
+    if (pov == 135.0) {
+      m_SwerveSubsystem.setDriveMode(driveMode.PIVOTFL);
+    }
+
+    if (pov == 225.0) {
+      m_SwerveSubsystem.setDriveMode(driveMode.PIVOTFR);
+    }
+
+    if (pov == 315.0 ) {
+      m_SwerveSubsystem.setDriveMode(driveMode.PIVOTBR);
+    }
+    */
+
+    m_SwerveSubsystem.drive(forwardCommand.getAsDouble() * SwerveSubsystemConstants.MAX_ACCEL_FEET_PER_SECOND, strafeCommand.getAsDouble() * SwerveSubsystemConstants.MAX_ACCEL_FEET_PER_SECOND, spinCommand.getAsDouble() * 25 /* inches */); //}
+  }
+
+  @Override
+  public boolean isFinished() { return false; }
+
+  @Override
+  public void end(boolean interrupted) {
+    //if(wasFieldCentric) {
+
+    m_SwerveSubsystem.setDriveMode(driveMode.DRIVE);
+  }
+}
